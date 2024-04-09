6e3ed4f8792d7ed2c0950589cb58dc0e1f9c4aa4
Daniel Faustino <daniel@dcx.ufpb.br>
2024-04-09 16:31:13 -0300

    (refact) change app to work with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 
 function App() {
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://react.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
